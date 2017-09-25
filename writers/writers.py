165ae2c75c89ac406f626582dfbab154ecbb6e7a
Chris Julian <chris@zero1many.net>
2017-09-25 19:42:28 -0400

    Supply writer classes

---
+++ writers/writers.py
@@ -0,0 +1,18 @@
+class KafkaWriter(object):
+
+    def __init__(self, config):
+        # read config
+        pass
+    
+    def write(self, packet):
+        # write packet to kafka
+        pass
+
+class StdoutWriter(object):
+
+    def __init__(self):
+        pass
+    
+    def write(self, packet):
+        # write packet to kafka
+        pass

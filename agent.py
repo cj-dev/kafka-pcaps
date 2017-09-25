4d2debc68b1e844c3622fd857a6f92ebf483099f
Chris Julian <chris@zero1many.net>
2017-09-25 19:42:16 -0400

    Parse config output preferences

--- agent.py
+++ agent.py
@@ -3,10 +3,37 @@ from pprint import pprint
 import yaml
 from scapy.all import sniff
 
-with open("config.yml") as cf:
-    config = yaml.load(cf, Loader=yaml.CLoader)
+from writers.writers import KafkaWriter, StdoutWriter
 
-print "Running with config \n{0}".format(yaml.dump(config))
+def write_packet(writers, packet):
+    pass
 
-captured = sniff(filter='tcp', count=2)
-print captured.nsummary()
+
+def select_writers(config):
+    writers = []
+
+    # Currently, strings or dictionary items in output config list
+    for output_method in config['agent']['output']:
+        if type(output_method) == str:
+            if output_method == "stdout":
+                chosen_writer = StdoutWriter()
+        else:
+            kafka_config = output_method.get('kafka', False)
+            if kafka_config:
+                chosen_writer = KafkaWriter(kafka_config)
+                writers.append(chosen_writer)
+        writers.append(chosen_writer)
+
+    return writers
+
+if __name__ == "__main__":
+    with open("config.yml") as cf:
+        config = yaml.load(cf, Loader=yaml.CLoader)
+
+    print "Running with config \n{0}".format(yaml.dump(config))
+
+    select_writers(config)
+
+    captured = sniff(filter='tcp', count=2)
+    print captured.nsummary()
+

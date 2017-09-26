4793634a690eaf6868e9c2eddfde0251f326e824
Chris Julian <chris@zero1many.net>
2017-09-26 03:33:54 -0400

    Basic Kafka consumer

---
+++ analyst.py
@@ -0,0 +1,24 @@
+import yaml
+from kafka import KafkaConsumer
+from scapy.all import Ether
+
+if __name__ == "__main__":
+
+    with open("config.yml") as cf:
+        config = yaml.load(cf, Loader=yaml.CLoader)
+
+    # Will I ever have more than one input? probably not...
+    for input_method in config['analyst']['input']:
+        kafka_config = input_method.get('kafka', None)
+
+    if not kafka_config:
+        print "Go define a config in the yaml"
+        exit(1)
+
+    host = config.get('host', '127.0.0.1')
+    port = config.get('port', 9092)
+    server_string = "{host}:{port}".format(host=host, port=port)
+
+    consumer = KafkaConsumer('dns_packets', bootstrap_servers=server_string)
+    for packet in consumer:
+        print Ether(packet)

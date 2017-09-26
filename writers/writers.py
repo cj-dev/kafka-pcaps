5c9cd68b1cfcfe0baac393a135904dfd9cead8c3
Chris Julian <chris@zero1many.net>
2017-09-26 00:50:34 -0400

    Initialize a kafka producer

--- writers/writers.py
+++ writers/writers.py
@@ -1,12 +1,17 @@
+from kafka import KafkaProducer
+
 class KafkaWriter(object):
 
     def __init__(self, config):
-        # read config
-        pass
+        host = config.get('host', '127.0.0.1')
+        port = config.get('port', 9092)
+        server_string = "{host}:{port}".format(host=host, port=port)
+        self.producer = KafkaProducer(bootstrap_servers=server_string)
     
-    def write(self, packet):
-        # write packet to kafka
+    def write(self, packet, topic):
         pass
+        # future = self.producer.send(topic, b'test_message')
+        # result = future.get(timeout=30)
 
 class StdoutWriter(object):
 

dd5468a880e6cc3b96f8b3aecef92e82f33ff6ac
Chris Julian <chris@zero1many.net>
2017-10-10 22:28:55 -0400

    Context needs 2 threads. Fix a bunch with the kafka stream

--- spark/sizeup.py
+++ spark/sizeup.py
@@ -10,20 +10,27 @@ from pyspark.streaming.kafka import KafkaUtils
 
 from scapy.all import *
 
+def packetmap(packet):
+    """ Flatmap packet length keyed by tuple of source and destination IPs """
+    if IP in packet:
+        return [(packet[IP].src, packet[IP].dst), len(packet)]
+    else:
+        return []
+
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Usage: kafka_wordcount.py <zk> <topic>", file=sys.stderr)
 	exit(-1)
 
-    sc = SparkContext(appName="PythonStreamingKafkaPCAPSizeup")
+    sc = SparkContext("local[2]", appName="PythonStreamingKafkaPCAPSizeup")
     sc.setLogLevel("WARN")
     ssc = StreamingContext(sc, 1)
 
     quorum, topic = sys.argv[1:]
 
-    stream = KafkaUtils.createStream(ssc, quorum, "Spark-streaming-consumer", {topic: 1})
-    pcaps = stream.map(lambda x: x[1])
-    sizes = pcaps.flatMap(lambda pcap: ((pcap['IP'].src, pcap['IP'].dst), len(pcap)))
+    stream = KafkaUtils.createStream(ssc, quorum, "Spark-streaming-consumer", {topic: 1}, valueDecoder=lambda x: x)
+    pcaps = stream.map(lambda x: Ether(x[1]))
+    sizes = pcaps.flatMap(packetmap)
     reduced_sizes = sizes.reduceByKey(lambda a, b: a+b)
 
     reduced_sizes.pprint()
